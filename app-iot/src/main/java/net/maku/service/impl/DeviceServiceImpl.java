69b2bd338cca6c91e841c1b508fb985030ea06b9
野路子 <2317413623@qq.com>
2025-06-06 12:30:28 +0800

    小程序设备-部分

---
+++ app-iot/src/main/java/net/maku/service/impl/DeviceServiceImpl.java
@@ -0,0 +1,95 @@
+package net.maku.service.impl;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import net.maku.convert.DeviceConvert;
+import net.maku.dao.DeviceDao;
+import net.maku.entity.Device;
+import net.maku.framework.common.constant.Constant;
+import net.maku.framework.common.exception.ServerException;
+import net.maku.framework.common.utils.PageResult;
+import net.maku.framework.mybatis.service.impl.BaseServiceImpl;
+import net.maku.query.DeviceQuery;
+import net.maku.service.DeviceService;
+import net.maku.vo.DeviceVO;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class DeviceServiceImpl extends BaseServiceImpl<DeviceDao, Device> implements DeviceService {
+    private final MessageChannel mqttOutboundChannel;
+
+    @Override
+    public PageResult<DeviceVO> page(DeviceQuery query) {
+        Map<String, Object> params = getParams(query);
+        IPage<Device> page = getPage(query);
+        params.put(Constant.PAGE, page);
+        List<Device> list = baseMapper.getList(params);
+        return new PageResult<>(DeviceConvert.INSTANCE.convertList(list), page.getTotal());
+    }
+
+    private Map<String, Object> getParams(DeviceQuery query) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", query.getName());
+        params.put("type", query.getType());
+        params.put("running_status", query.getRunningStatus());
+        return params;
+    }
+
+    @Override
+    public void sendCommand(String uid, String command) {
+        QueryWrapper<Device> query = new QueryWrapper<>();
+        query.eq("uid", uid);
+        Device device = this.getOne(query);
+        if (device == null) {
+            throw new ServerException("设备不存在");
+        }
+        // 构建json命令
+        Map<String, Object> map = new HashMap<>();
+        map.put("uid", uid);
+        map.put("command", command);
+        String payload = JSON.toJSONString(map);
+        Message<String> message = MessageBuilder.withPayload(payload)
+                .setHeader("mqtt_topic", "device/" + uid + "/control")
+                .build();
+        mqttOutboundChannel.send(message);
+    }
+
+    // 处理状态上报
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public void handleStatusMessage(Message<?> message) {
+        String payload = message.getPayload().toString();
+        try {
+            JSONObject json = JSON.parseObject(payload);
+            String uid = json.getString("uid");
+            Boolean runningStatus = json.getBoolean("running_status");
+            Float temperature = json.getFloat("temperature");
+            Float humidity = json.getFloat("humidity");
+            // 更新数据库状态
+            UpdateWrapper<Device> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.eq("uid", uid)
+                    .set("running_status", runningStatus)
+                    .set("temperature", temperature)
+                    .set("humidity", humidity);
+            baseMapper.update(null, updateWrapper);
+            log.info("设备状态更新：{} -> {},{},{}", uid, runningStatus,temperature,humidity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
